492c7bb05a5acf8b5c5727f877bafb0f86032e83
Arfat Kadvekar <arfatkadvekar1305@gmail.com>
2025-02-14 15:12:19 +0530

    version 2

--- toralizer.c
+++ toralizer.c
@@ -1,16 +1,142 @@
+// toralizer.c
 #include "toralizer.h"
 
-int main(int argc, char *argv[])
-{
-    char *host;
-    int port;
+int create_connection(const char *host, int port) {
+    int sockfd;
+    struct sockaddr_in proxy_addr;
+    
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        handle_error("Failed to create socket");
+        return -1;
+    }
+    
+    // Set up proxy address
+    memset(&proxy_addr, 0, sizeof(proxy_addr));
+    proxy_addr.sin_family = AF_INET;
+    proxy_addr.sin_port = htons(PROXYPORT);
+    if (inet_pton(AF_INET, PROXY, &proxy_addr.sin_addr) <= 0) {
+        handle_error("Invalid proxy address");
+        cleanup_socket(sockfd);
+        return -1;
+    }
+    
+    // Connect to proxy
+    if (connect(sockfd, (struct sockaddr*)&proxy_addr, sizeof(proxy_addr)) < 0) {
+        handle_error("Failed to connect to proxy");
+        cleanup_socket(sockfd);
+        return -1;
+    }
+    
+    return sockfd;
+}
 
-    if (argc < 3)
-    {
+int send_proxy_request(int sockfd, const char *host, int port) {
+    Req request;
+    struct in_addr addr;
+    
+    // Initialize request structure
+    memset(&request, 0, sizeof(request));
+    request.vn = SOCKS_VERSION;
+    request.cd = CONNECT_COMMAND;
+    request.dstport = htons(port);
+    
+    // Resolve hostname and set destination IP
+    if (resolve_host(host, &addr) < 0) {
+        handle_error("Failed to resolve hostname");
+        return -1;
+    }
+    request.dstip = addr.s_addr;
+    
+    // Send request to proxy
+    if (send(sockfd, &request, sizeof(request), 0) != sizeof(request)) {
+        handle_error("Failed to send request to proxy");
+        return -1;
+    }
+    
+    return 0;
+}
+
+int receive_proxy_response(int sockfd) {
+    Res response;
+    
+    // Receive response from proxy
+    if (recv(sockfd, &response, sizeof(response), 0) != sizeof(response)) {
+        handle_error("Failed to receive proxy response");
+        return -1;
+    }
+    
+    // Check response
+    if (response.vn != 0 || response.cd != 90) {
+        fprintf(stderr, "Proxy request failed with code: %d\n", response.cd);
+        return -1;
+    }
+    
+    return 0;
+}
+
+int resolve_host(const char *host, struct in_addr *addr) {
+    struct hostent *hp;
+    
+    if (inet_aton(host, addr) != 0) {
+        return 0;  // Successfully converted numeric IP
+    }
+    
+    hp = gethostbyname(host);
+    if (!hp) {
+        return -1;
+    }
+    
+    memcpy(addr, hp->h_addr_list[0], hp->h_length);
+    return 0;
+}
+
+void handle_error(const char *msg) {
+    fprintf(stderr, "Error: %s - %s\n", msg, strerror(errno));
+}
+
+void cleanup_socket(int sockfd) {
+    if (sockfd >= 0) {
+        close(sockfd);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *host;
+    int port, sockfd;
+    
+    if (argc < 3) {
         fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
         return -1;
     }
-
+    
     host = argv[1];
     port = atoi(argv[2]);
+    
+    // Create connection to proxy
+    sockfd = create_connection(host, port);
+    if (sockfd < 0) {
+        return -1;
+    }
+    
+    // Send proxy request
+    if (send_proxy_request(sockfd, host, port) < 0) {
+        cleanup_socket(sockfd);
+        return -1;
+    }
+    
+    // Receive and verify proxy response
+    if (receive_proxy_response(sockfd) < 0) {
+        cleanup_socket(sockfd);
+        return -1;
+    }
+    
+    printf("Successfully connected to %s:%d through SOCKS4 proxy\n", host, port);
+    
+    // At this point, the connection is established and ready for data transfer
+    // You can add your data transfer logic here
+    
+    cleanup_socket(sockfd);
+    return 0;
 }
--- toralizer.h
+++ toralizer.h
@@ -1,36 +1,54 @@
+// toralizer.h
+#ifndef TORALIZER_H
+#define TORALIZER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-
-socket()
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 #define PROXY "127.0.0.1"
 #define PROXYPORT 9050
+#define SOCKS_VERSION 4
+#define CONNECT_COMMAND 1
+#define BUFFER_SIZE 8192
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
-struct proxy_request
-{
-    int8 vn;
-    int8 cd;
-    int16 dstport;
-    int32 dstip;
-
-    unsigned char userid[8];
+// SOCKS4 request structure
+struct proxy_request {
+    int8 vn;        // SOCKS version number
+    int8 cd;        // Command code
+    int16 dstport;  // Destination port
+    int32 dstip;    // Destination IP
+    unsigned char userid[8];  // User ID field
 };
 typedef struct proxy_request Req;
 
-struct proxy_response
-{
-    int8 vn;
-    int8 cd;
-    int16 _;
-    int32 __;
+// SOCKS4 response structure
+struct proxy_response {
+    int8 vn;        // Version number
+    int8 cd;        // Response code
+    int16 dstport;  // Destination port (ignored)
+    int32 dstip;    // Destination IP (ignored)
 };
-
 typedef struct proxy_response Res;
+
+// Function prototypes
+int create_connection(const char *host, int port);
+int send_proxy_request(int sockfd, const char *host, int port);
+int receive_proxy_response(int sockfd);
+void handle_error(const char *msg);
+int resolve_host(const char *host, struct in_addr *addr);
+void cleanup_socket(int sockfd);
+
+#endif // TORALIZER_H
